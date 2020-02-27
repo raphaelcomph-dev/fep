2d9513307a313a2c515b3035f184ab2aa6a7b516
Raphael Soares <raphael.soares@pitang.com>
2020-02-27 14:29:50 -0300

    Criando o componente da seção de Infraestrutura;

---
+++ mostra-espirita/src/app/components/infra/component_infra.controller.js
@@ -0,0 +1,18 @@
+angular.module("app").component("infraComponent", {
+	templateUrl: "/src/app/components/infra/component_infra.html",
+	controller: [InfraController]
+});
+
+function InfraController() {
+	// var $ctrl = this;
+	//variables declaration
+	//$ctrl.someVariable;
+	//Controller methods
+	//#region
+	//$ctrl.someFunction = function(params) {}
+	//#endregion
+	//private methods
+	//#region
+	//function _otherFunction(params) {}
+	//#endregion
+}
