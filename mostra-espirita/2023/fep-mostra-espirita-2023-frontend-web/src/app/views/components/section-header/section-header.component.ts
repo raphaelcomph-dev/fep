f552eea9bce99a102c9082a0f3b96f68d9e67971
Raphael Duarte Soares <118555152+raphaelduartesoares@users.noreply.github.com>
2023-05-05 15:19:45 -0300

    feat: sticky header when scrolling

--- mostra-espirita/2023/fep-mostra-espirita-2023-frontend-web/src/app/views/components/section-header/section-header.component.ts
+++ mostra-espirita/2023/fep-mostra-espirita-2023-frontend-web/src/app/views/components/section-header/section-header.component.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-section-header',
-  templateUrl: './section-header.component.html',
-  styleUrls: ['./section-header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: "app-section-header",
+    templateUrl: "./section-header.component.html",
+    styleUrls: ["./section-header.component.scss"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionHeaderComponent {
+    isHeaderFixed = false;
 
+    @HostListener("window:scroll", ["$event"])
+    onWindowScroll() {
+        if (window.scrollY >= 80) {
+            console.log("fixing the header");
+
+            this.isHeaderFixed = true;
+        } else {
+            console.log("releasing the header");
+            this.isHeaderFixed = false;
+        }
+    }
 }
