a94d2cae6aa1b90e92f5af963de740b104115c8c
Raphael Duarte Soares <118555152+raphaelduartesoares@users.noreply.github.com>
2023-05-11 11:59:48 -0300

    feat: adds 'subcribe to my agenda' button on Schedule section

--- mostra-espirita/2023/fep-mostra-espirita-2023-frontend-web/src/app/views/components/section-schedule/section-schedule.component.ts
+++ mostra-espirita/2023/fep-mostra-espirita-2023-frontend-web/src/app/views/components/section-schedule/section-schedule.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { fadeInOut } from "../../../../assets/animations/fadeInOut";
 
 @Component({
@@ -10,4 +10,22 @@ import { fadeInOut } from "../../../../assets/animations/fadeInOut";
 })
 export class SectionScheduleComponent {
     showDay = 1;
+
+    urlCalendarEventsApple =
+        "webcal://calendar.google.com/calendar/ical/ec59397d35d6631f2ee99c77f8153d2da6233010eb1e4ca53fed80801c500b95@group.calendar.google.com/public/basic.ics";
+    urlCalendarEventsGoogle =
+        "https://calendar.google.com/calendar/render?cid=ec59397d35d6631f2ee99c77f8153d2da6233010eb1e4ca53fed80801c500b95@group.calendar.google.com";
+
+    urlCalendarEvents = "";
+
+    SMALL_SCREEN_BREAKPOINT = 576;
+
+    constructor() {
+        const userAgent = navigator.userAgent;
+        if (userAgent.indexOf("iPhone") !== -1) {
+            this.urlCalendarEvents = this.urlCalendarEventsApple;
+        } else {
+            this.urlCalendarEvents = this.urlCalendarEventsGoogle;
+        }
+    }
 }
